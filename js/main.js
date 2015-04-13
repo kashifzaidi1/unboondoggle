8bab873750607ff3120a21ef8d02d1fdb4b093be
Kashif Zaidi <macbookpro@Personal-Macbook-Loops.local>
2015-04-13 19:44:35 +0500

    v1

---
+++ js/main.js
@@ -0,0 +1,306 @@
+var data = {
+	"Monaco": "89.52",
+	"Japan": "84.74",
+	"Singapore": "84.68",
+	"Macau": "84.51",
+	"San Marino": "83.24",
+	"Andorra": "82.72",
+	"Guernsey": "82.47",
+	"Hong Kong": "82.37",
+	"Australia": "82.15",
+	"Italy": "82.12",
+	"Liechtenstein": "81.77",
+	"Channel Islands": "81.76",
+	"Canada": "81.76",
+	"France": "81.6",
+	"Spain": "81.57",
+	"Sweden": "81.48",
+	"Switzerland": "81.48",
+	"Israel": "81.38",
+	"Iceland": "81.32",
+	"Anguilla": "81.31",
+	"Netherlands": "81.23",
+	"Bermuda": "81.15",
+	"Cayman Islands": "81.13",
+	"Isle of Man": "81.09",
+	"New Zealand": "81.05",
+	"Norway": "80.69",
+	"Ireland": "80.68",
+	"Germany": "80.57",
+	"Jordan": "80.54",
+	"United Kingdom": "80.54",
+	"Greece": "80.43",
+	"Saint Pierre and Miquelon": "80.39",
+	"Austria": "80.3",
+	"Malta": "80.25",
+	"Faroe Islands": "80.24",
+	"Luxembourg": "80.14",
+	"Belgium": "80.05",
+	"South Korea": "80.04",
+	"U.S. Virgin Islands": "79.89",
+	"Finland": "79.84",
+	"Turks and Caicos Islands": "79.69",
+	"Wallis and Futuna": "79.57",
+	"Puerto Rico": "79.5",
+	"Saint Barthélemy": "79.48",
+	"Saint Martin": "79.48",
+	"Bosnia and Herzegovina": "79.41",
+	"Saint Helena": "79.36",
+	"Gibraltar": "79.28",
+	"Denmark": "79.25",
+	"Portugal": "79.16",
+	"Guam": "78.98",
+	"Taiwan": "78.97",
+	"United States of America": "78.88",
+	"Bahrain": "78.73",
+	"Qatar": "78.59",
+	"Cyprus": "78.51",
+	"Panama": "78.47",
+	"British Virgin Islands": "78.46",
+	"Montenegro": "78.43",
+	"Costa Rica": "78.4",
+	"Cuba": "78.39",
+	"Chile": "78.39",
+	"Libya": "78.34",
+	"Mayotte": "78.19",
+	"Netherlands Antilles": "78.16",
+	"Albania": "78.13",
+	"Slovenia": "78.01",
+	"Dominican Republic": "77.97",
+	"Sint Maarten": "77.97",
+	"Czech Republic": "77.92",
+	"Georgia": "77.89",
+	"Northern Mariana Islands": "77.82",
+	"Kuwait": "77.82",
+	"Argentina": "77.69",
+	"Saint Lucia": "77.6",
+	"New Caledonia": "77.5",
+	"United Arab Emirates": "77.29",
+	"Mexico":	"77.25",
+	"Uruguay":	"77",
+	"Paraguay": "76.99",
+	"French Polynesia": "76.98",
+	"Brunei": "76.97",
+	"Poland": "76.85",
+	"Dominica": "76.79",
+	"Morocco": "76.71",
+	"Slovakia": "76.65",
+	"Croatia": "76.61",
+	"Ecuador": "76.56",
+	"Aruba": "76.56",
+	"Sri Lanka": "76.56",
+	"Antigua and Barbuda": "76.33",
+	"Lithuania": "76.19",
+	"Tonga": "76.04",
+	"Macedonia": "76.02",
+	"West Bank": "75.91",
+	"Lebanon": "75.9",
+	"Tunisia": "75.89",
+	"Hungary": "75.69",
+	"Cook Islands": "75.6",
+	"Syria": "75.59",
+	"Saint Kitts and Nevis": "75.52",
+	"Colombia": "75.48",
+	"Algeria": "75.41",
+	"Mauritius": "75.4",
+	"Maldives": "75.37",
+	"China": "75.3",
+	"Serbia": "75.26",
+	"Oman": "75.21",
+	"Barbados": "75.18",
+	"American Samoa": "75.14",
+	"Solomon Islands": "75.12",
+	"Saint Vincent and the Grenadines": "75.09",
+	"Saudi Arabia": "75.05",
+	"Romania": "74.92",
+	"Gaza Strip": "74.87",
+	"Malaysia": "74.75",
+	"Bulgaria": "74.57",
+	"Venezuela": "74.54",
+	"Thailand": "74.5",
+	"Seychelles": "74.49",
+	"El Salvador": "74.42",
+	"Estonia": "74.31",
+	"Armenia": "74.27",
+	"Montserrat": "74.14",
+	"Grenada": "74.05",
+	"Latvia": "73.7",
+	"Egypt": "73.7",
+	"Brazil": "73.57",
+	"Jamaica": "73.55",
+	"Uzbekistan": "73.55",
+	"Turkey": "73.55",
+	"Peru": "73.48",
+	"Samoa": "73.46",
+	"Vietnam": "73.16",
+	"Nicaragua": "72.98",
+	"Palau": "72.87",
+	"Marshall Islands": "72.84",
+	"Philippines": "72.75",
+	"Federated States of Micronesia": "72.62",
+	"Trinidad and Tobago": "72.59",
+	"Indonesia": "72.45",
+	"Fiji": "72.43",
+	"Belarus": "72.3",
+	"The Bahamas": "72.2",
+	"Azerbaijan": "72.2",
+	"Greenland": "72.1",
+	"Guatemala": "72.02",
+	"Suriname": "71.97",
+	"Cape Verde": "71.85",
+	"Iraq": "71.7",
+	"Kosovo": "71.33",
+	"Kyrgyzstan": "71.21",
+	"Iran":	"71.15",
+	"Honduras":	"71",
+	"Bangladesh": "70.94",
+	"Kazakhstan": "70.55",
+	"Moldova": "70.42",
+	"North Korea": "70.11",
+	"Turkmenistan": "69.78",
+	"Mongolia": "69.57",
+	"Bhutan": "69.51",
+	"Ukraine": "69.39",
+	"East Timor": "69.22",
+	"Bolivia": "68.86",
+	"Belize": "68.59",
+	"Guyana": "68.21",
+	"India": "68.13",
+	"Nepal": "67.52",
+	"Tajikistan": "67.39",
+	"Pakistan": "67.39",
+	"Russia": "67.15",
+	"Papua New Guinea": "67.03",
+	"Nauru": "66.75",
+	"Myanmar": "66.29",
+	"Tuvalu": "66.16",
+	"Vanuatu": "66.11",
+	"Kiribati": "65.81",
+	"Yemen": "65.18",
+	"Madagascar": "65.08",
+	"The Gambia": "64.6",
+	"Sao Tome and Principe": "64.58",
+	"Togo": "64.51",
+	"Cambodia": "64.14",
+	"Laos": "63.88",
+	"Comoros": "63.85",
+	"Equatorial Guinea": "63.85",
+	"Eritrea": "63.81",
+	"Kenya": "63.77",
+	"Haiti": "63.51",
+	"Ghana": "62.89",
+	"Djibouti": "62.79",
+	"Mauritania": "62.65",
+	"Western Sahara": "62.64",
+	"Benin": "61.47",
+	"Senegal": "61.32",
+	"Burundi": "60.54",
+	"Guinea": "60.08",
+	"Sudan": "59.8",
+	"Rwanda": "59.67",
+	"Liberia": "58.6",
+	"Cote d'Ivoire": "58.34",
+	"Sierra Leone": "57.79",
+	"Ethiopia": "57.64",
+	"Zimbabwe": "57.05",
+	"Democratic Republic of the Congo": "56.93",
+	"Republic of Congo": "56.2",
+	"Angola": "55.63",
+	"Cameroon": "55.59",
+	"Burkina Faso": "55.12",
+	"Niger": "54.98",
+	"Mali": "54.38",
+	"Botswana": "54.18",
+	"Uganda": "54.04",
+	"Tanzania": "53.78",
+	"Malawi": "53.53",
+	"Nigeria": "53.26",
+	"Zambia": "53.16",
+	"Mozambique": "52.94",
+	"Lesotho": "52.86",
+	"Gabon": "52.04",
+	"Somalia": "51.96",
+	"Central African Republic": "51.81",
+	"Namibia": "51.62",
+	"Swaziland": "51.05",
+	"Afghanistan": "50.87",
+	"Guinea-Bissau": "50.23",
+	"Chad": "49.81",
+	"South Africa": "49.72"
+};
+
+var local = {
+	getBirthDay : function(){
+		return (typeof localStorage.birth_day !== "undefined")? localStorage.birth_day : false;
+	},
+	setBirthDay : function(birth_day){
+		localStorage.birth_day = birth_day;
+	},
+	getCountry : function(){
+		return (typeof localStorage.country !== "undefined")? localStorage.country : false;
+	},
+	setCountry : function(country){
+		localStorage.country = country;
+	},
+};
+
+var age = null;
+var driver = {
+
+	getAgeByCountry : function(country){
+		return typeof data[country] !== "undefined" ? data[country] : false;
+	},
+
+	init : function(){
+		$('.timer, .country, .dob').hide();
+		var birth_day = local.getBirthDay(), 
+			country = local.getCountry();
+		
+		$('.'+(birth_day ? (country)? 'timer': 'country' : 'dob')).fadeIn('slow');
+		driver.start(birth_day, country);
+	},
+
+
+	start: function(birth_day, country){
+		if(birth_day && country){
+			setInterval(function(){
+				age = driver.getAgeByCountry(country);
+				var birthday = moment(local.getBirthDay());
+				var deathday = birthday.clone();
+				deathday.add(age,'years');
+				var years = deathday.diff(moment().add(1,'years'),'years');
+				var seconds_left_in_this_year = moment().endOf('year').diff(moment());
+				$('.ageAge').text(years);
+				$('.ageSeconds').text(seconds_left_in_this_year)
+			}, 100);
+		}
+	}
+};
+
+
+$(document).ready(function () {
+   
+    driver.init();
+
+    // set datepicker
+    $('#datetimepicker1').datetimepicker({
+    	dayViewHeaderFormat : 'MMMM YYYY',
+    	defaultDate : moment().format(),
+    	format : 'MM/DD/YYYY',
+    	viewMode : 'years'
+    	
+    });
+
+    // when date changes
+    $('#datetimepicker1').on('dp.change',function(e){
+    	e.preventDefault();
+    	local.setBirthDay($(this).data("DateTimePicker").date().format());
+    	driver.init();
+    	return false;
+    });
+
+    $("#country").on('change',function(){
+    	local.setCountry($(this).val());
+    	driver.init();
+    });
+});
